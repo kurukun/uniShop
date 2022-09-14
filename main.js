c32b76011b96ce9143c036f2166c9285d6110430
kuru <kurukun@qq.com>
2022-09-14 12:40:09 +0800

    转移到工位电脑进行开发

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入网络请求的包
+import {
+  $http
+} from '@escook/request-miniprogram'
+uni.$http = $http
 
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+// 配置请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,8 +8,21 @@
   export default {
     data() {
       return {
-
+        // 这是轮播图的数据列表
+        swiperList: []
       };
+    },
+    onLoad() {
+      // 调用方法，获取轮播图的数据
+      this.getSwiperList();
+    },
+    methods: {
+      async getSwiperList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata');
+        console.log(res);
+      }
     }
   }
 </script>
